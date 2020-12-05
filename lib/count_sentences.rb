10ae3c751a6f1fed6f85b6a347d5df954fbd3b3a
Moomal <moomalmsoomro@gmail.com>
2020-12-04 23:02:08 -0500

    commit tree ecfb25e90687dfeab9b693792fd1b90c8095b08c

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    a = self.split(/[.?!]/)
+    a.delete("")
+    a.count
   end
 end
